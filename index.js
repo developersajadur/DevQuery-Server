718522726eabda286e6715eda48c1f0b7a184407
itzmesojib <itzmesojib@gmail.com>
2024-10-13 15:38:15 +0600

    added

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const io = new Server(server, {
 });
 
 // MongoDB connection
-const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
